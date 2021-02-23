32779e429883559ffb14e7eaa914780762724351
Tri <nitylims@gmail.com>
2021-02-23 21:00:19 +0700

    to be deleted

---
+++ client.php
@@ -0,0 +1,17 @@
+<?
+    echo "Hi there"
+?>
+
+<html>
+    <head>
+    	<link rel="stylesheet" href="style.css">
+        <title>Under Construction</title>
+        <div id = "heading">
+	        <h1>Hello world, this is an up-and-coming anonymous web chat app.</h1>
+	        <p id = "hello">Here comes the lorem ipsum.</p>
+    	</div>
+    </head>
+    <body>
+        
+    </body>
+</html>
