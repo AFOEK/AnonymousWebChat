e209bb468fea4bde0b7ffc6e38e7fc45125fffac
AFOEK <felix.montalfu@gmail.com>
2021-04-02 15:21:26 +0700

    Simple server.js script

--- server.js
+++ server.js
@@ -13,6 +13,40 @@ mongo.connect('mongodb://127.0.0.1/anonchat', function(err, db){
     console.log('Databases is already created...');
 
     client.on('connection', function(socket){
-        
-    })
-})
+        let chat = db.collection('chats');
+        sendStat = function(s){
+            socket.emit('status',s);
+        }
+
+        chat.find().limit(250).sort({_id:1}).toArray(function(err,res){
+            if(err){
+                throw err;
+            }
+
+            socket.emit('ouput', res);
+        });
+
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+
+            if(name== '' || message==''){
+                sendStat('Please enter a name and message');
+            }else{
+                chat.insert({name: name, message: message}, function(){
+                    client.emit('output', [data]);
+
+                    sendStat({
+                        message: 'Message sent',
+                        clear: true
+                    });
+                });
+            }
+        });
+        socket.on('clear', function(data){
+            chat.remove({}, function(){
+                socket.emit('Cleared');
+            });
+        });
+    });
+});
