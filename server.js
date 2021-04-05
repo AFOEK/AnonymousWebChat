983505408954bc122e4d7705932773f508a4595e
AFOEK <felix.montalfu@gmail.com>
2021-04-05 11:31:59 +0700

    adding library

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 
-
 mongo.connect('mongodb://127.0.0.1/anonchat', function(err, db){
     if(err){
         throw err;
