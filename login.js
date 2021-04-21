a12dbcdf6671417f776b641a94b1d44de9d94695
AFOEK <felix.montalfu@gmail.com>
2021-04-21 21:29:17 +0700

    building user login backend

--- login.js
+++ login.js
@@ -4,9 +4,25 @@ var passport =  require("passport");
 var bodyParser =  require("body-parser");
 var LocalStrat = require("passport-local");
 var passport_local_mongoose = require("passport-local-mongoose");
+var User = require('./models/user');
 
 mongoose.set('useNewUrlParser',true);
 mongoose.set('useFindandModify',true);
 mongoose.set('useCreateIndex',true);
 mongoose.set('useUnifiedTopology',true);
 mongoose.connect('mongodb://127.0.0.1/anonchat');
+
+var app=express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Do it 4 her !",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrat(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
